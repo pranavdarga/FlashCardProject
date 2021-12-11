1faa86b0852b6e312e6cb6438e90ecdd9de06a9d
shatayu <shatayukulkarni@gmail.com>
2021-12-11 00:55:13 -0500

    removed unneeded comments

--- application.py
+++ application.py
@@ -14,75 +14,11 @@ CORS(app)
 INVALID_USER_ID = -1
 INT_MAX_SQL = 2147483647
 
-#set FLASK_APP=application.py
-#set FLASK_ENV=development
-#python
-#from application import db
-#db.create_all()
-#from application import <tables>
-#card1 = cards(cardname = "Chemistry", deckid = 2, question = "What is H2O?", answer = "Water")
-#deck1 = Decks(deckname = "English", userid = 23)
-#db.session.add(<row>)
-#db.session.commit()
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-error_list = []
-
-class accounts(db.Model):
-    userid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    name = db.Column(db.String(80), nullable=False)
-
-class Decks(db.Model):
-    deckid = db.Column(db.Integer, primary_key=True)
-    deckname = db.Column(db.String(80), nullable=False)
-    userid = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"{self.deckname} - {self.userid}"
-
-class deckUsers(db.Model):
-    userid = db.Column(db.Integer, primary_key=True)
-    deckid = db.Column(db.Integer, primary_key=True)
-    lasttime = db.Column(db.DateTime, nullable=False)
-
-class cards(db.Model):
-    cardid = db.Column(db.Integer, primary_key=True)
-    cardname = db.Column(db.String(80), nullable=False)
-    deckid = db.Column(db.Integer, nullable=False)
-    question = db.Column(db.String(200), nullable=False)
-    answer = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f"{self.cardname} - {self.question} - {self.answer}"
-
-class cardHistory(db.Model):
-    cardid = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, primary_key=True)
-    userid = db.Column(db.Integer, primary_key=True)
-
 @app.route('/')
 def index():
     return """
         Use this API to make requests for our Cards DataBase
     """
-#
-# #encrypted username/password GET
-# #response: userhistory if successful else return no such user
-# @app.route('/auth/<username>/<password>')
-# def auth(username, password):
-#     return {"userid" : username, "password" : password}
-#
-# #create new user POST
-# #response: return success
-# @app.route('/register/<username>/<password>')
-# def register_user(username, password):
-#     return {"errors" : error_list}
-#
-# #find a deck GET
-# #response: deck and all the associated cards or NoSuchDeck
 
 config = {
     'user': 'root',
@@ -135,31 +71,6 @@ def get_deck(deckid):
 
     return jsonify({"cards": output})
 
-    # # original
-    # deck = Decks.query.get_or_404(deckid)
-    # # card = cards.query.all()
-    # # output = []
-    # # for c in cards:
-    # #     card_data = {'cardname': card.cardname, 'question': card.question, 'answer': card.answer}
-    # #     output.append(card_data)
-
-    # cards1 = cards.query.all()
-    # output = []
-    # for card in cards1:
-    #     if int(card.deckid) == int(deckid):
-    #         deck_data = {'cardname': card.cardname, 'deckid': card.deckid, 'question': card.question, 'answer': card.answer}
-    #         output.append(deck_data)
-    # #return {"cards": output}
-    # return {"deckname": deck.deckname, "userid": deck.userid, "cards": output}
-
-    # return {"deckname": deck.deckname, "userid": deck.userid}
-#create a deck POST
-#response: success or unsuccessful
-#from application import <tables>
-#card1 = cards(cardname = "Chemistry", deckid = 2, question = "What is H2O?", answer = "Water")
-#deck1 = Decks(deckname = "English", userid = 23)
-#db.session.add(<row>)
-#db.session.commit()
 @app.route('/createdeck', methods=['POST'])
 def create_deck():
     userid = request.json['userid']
